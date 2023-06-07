94458e47d927f9d8eefe19a3f98b8f36f35e173a
Bhumikkalola21 <bhumik.kalola@bacancy.com>
2023-06-07 16:36:55 +0530

    typescript session

---
+++ src/views/AboutView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>About View</h2>
+    <p>{{ receivedMessage }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import moment from "moment";
+export default defineComponent({
+  name: "AboutView",
+  props: {
+    receivedMessage: {
+      type: String as PropType<string>,
+      required: true,
+    },
+  },
+  mounted() {
+    const now = moment();
+    console.log(now.format("YYYY-MM-DD"));
+  },
+});
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="app">
+    <p>{{ name }} - {{ age }}</p>
+    <button @click="changeName('heet')">change name</button>
+    <button @click="changeAge(87)">change age</button>
+    <AboutView :receivedMessage="message" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import AboutView from "./AboutView.vue";
+const age = 1;
+interface User {
+  name: string;
+  age: number;
+  email: string;
+}
+
+export default defineComponent({
+  name: "App",
+  components: {
+    AboutView,
+  },
+  props: { receivedMessage: { type: String, required: true } },
+
+  data() {
+    return {
+      message: "Hello how are you?" as string,
+      name: "Bhumik",
+      user: {} as User,
+      // users: [] as User[],
+      age: 22 as number | string,
+    };
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name;
+    },
+    changeAge(age: number | string) {
+      this.age = age;
+      return age;
+    },
+  },
+  mounted() {
+    // this.name = 23
+  },
+});
+</script>
+
+<style></style>
